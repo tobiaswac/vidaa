b89c7a0c2a87ebd98e22abf4e25d458b46109dc5
Tobias Wacker <tobiaswacker@gmail.com>
2013-09-04 20:05:37 +0200

    Created Vidaa

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vidaa::Application.config.secret_token = 'b6eb46b61eb82d8a5647c5ca386190a94339f87527b92b290c1eb6deceec559dae52a494b1aeb38f085967ac47a09fdf313f5b4882bf2862be328f6cfec6c770'
